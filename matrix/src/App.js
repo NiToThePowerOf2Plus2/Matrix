187dbf80ea92de890b198c9df5a6434800145c16
Hanen Alrwasheda <hanenalrwasheda.off@gmail.com>
2021-07-30 08:42:22 +0200

    simple logic created using normal divs instead of tables.
    styling is also simple and needs to be responsiv
    
    bug by pressing few times on button -> content is not being deleted to draw the new matrix!!!

--- matrix/src/App.js
+++ matrix/src/App.js
@@ -1,22 +1,71 @@
 import './App.css';
+import {useEffect, useState} from "react"
 
 function App() {
+  
+  
+  const [rows,setRows] = useState(0);
+  const [cols,setCols] = useState(0);
+  const resultStyle = {
+    alignContent: "center",
+    margin: 0,
+    padding: "20px",
+    display: "grid",
+    gridTemplate: `repeat(${rows},30px)/repeat(${cols},1fr)`
+  };
+  function getValue(){
+    let rowsVal = document.getElementById("rows");
+    let colsVal = document.getElementById("cols");
+    setRows(rowsVal.value);
+    setCols(colsVal.value);
+  }
+  function createMatrix(){
+    let rowsVal = document.getElementById("rows");
+    let colsVal = document.getElementById("cols");
+    if(rowsVal.value === "" || colsVal.value === "" || rowsVal.value === "0" || colsVal.value === "0"){
+      
+      alert("rows and columns must be greater than 0!");
+    }
+    for(let i=1; i<=rows; i++){
+      for(let j=1; j<=cols; j++){
+
+        let newElement = document.createElement("div");
+        newElement.setAttribute("id","cell");
+        let text = document.createTextNode(`${i}/${j}`);
+        newElement.appendChild(text);
+        let fatherElement = document.getElementById("result-container");
+        fatherElement.appendChild(newElement);
+        newElement.setAttribute("style",
+        `
+          grid-row:${i}/${i+1};
+          grid-col:${j}/${j+1};
+          margin: 2px;
+
+        `
+        )
+      }
+    }
+    
+  }
+  
   return (
     <div className="main-container">
       <div className="content-container">
         <div className="header-container">
-          <h1 className="header">BUILD OWN MATRIX</h1>
+          <h1 className="header">BUILD MATRIX</h1>
         </div>
         <div className="input">
           <label className="input-ele" for="rows">rows</label>
-          <input className="input-ele" id="rows" type="number"></input>
+          <input className="input-ele" id="rows" placeholder="number of rows" type="number" onChange={getValue}></input>
           <label className="input-ele" for="columns">columns</label>
-          <input className="input-ele" id="columns" type="number"></input>
+          <input className="input-ele" id="cols" placeholder="number of columns" type="number" onChange={getValue}></input>
         </div>
         <div className="btn-container">
-          <button className="btn">BUILD</button>
+          <button className="btn" onClick={createMatrix}>BUILD</button>
         </div>
-        <div className="result-container"></div>
+        <div id="result-container" style={resultStyle}>
+          
+        </div>
       </div>
     </div>
   );
